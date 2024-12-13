18835073bd1343ec3b76f218a633885ed9caf8e5
Celal <binaycelalselim@gmail.com>
2024-12-13 21:20:04 +0300

    email and phone added
    Co-authored-by: wiseweb-works <wisewebworks@outlook.com>
    Co-authored-by: yavuzahmet1 <1yavuzahmet@gmail.com>
    Co-authored-by: zbaharyilmaz <ylmzbhr1@gmail.com>
    Co-authored-by: nur2810 <sseymanurarslan@gmail.com>
    Co-authored-by: ImrenR <imrenrahbay@gmail.com>
    Co-authored-by: Celal-Dzelal <binaycelalselim@gmail.com>

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ veriGetir();
 const ekranaBas = (person) => {
   box.innerHTML = `
     <img class="image" src="${person.results[0].picture.large}" alt="" />
-    <h3>${person.results[0].name.title} ${person.results[0].name.first} ${person.results[0].name.last}</h3>`;
+    <h3>${person.results[0].name.title} ${person.results[0].name.first} ${person.results[0].name.last}</h3>
+    <h5>${person.results[0].email}</h5>
+    <h5>${person.results[0].phone}</h5>`;
 };
 button.addEventListener("click", (e) => {
   veriGetir();
