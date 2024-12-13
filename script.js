614668ad3d5eb4a0428ccd633e5d237764e77554
Celal <binaycelalselim@gmail.com>
2024-12-13 21:10:44 +0300

    Update
    Co-authored-by: wiseweb-works <wisewebworks@outlook.com>
    Co-authored-by: yavuzahmet1 <1yavuzahmet@gmail.com>
    Co-authored-by: zbaharyilmaz <ylmzbhr1@gmail.com>
    Co-authored-by: nur2810 <sseymanurarslan@gmail.com>
    Co-authored-by: ImrenR <imrenrahbay@gmail.com>
    Co-authored-by: Celal-Dzelal <binaycelalselim@gmail.com>

---
+++ script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".button");
+const box = document.querySelector(".box");
+
+const veriGetir = async () => {
+  const result = await fetch("https://randomuser.me/api/");
+  const data = await result.json();
+  console.log(data);
+  ekranaBas(data);
+};
+veriGetir();
+
+const ekranaBas = (person) => {
+  box.innerHTML = `
+    <img class="image" src="${person.results[0].picture.large}" alt="" />
+    <h3>${person.results[0].name.title} ${person.results[0].name.first} ${person.results[0].name.last}</h3>`;
+};
+button.addEventListener("click", (e) => {
+  veriGetir();
+});
